be234789caf9994a35feb792ca47dfd3c1a2ee9d
Hozafia <hozaifabinmasud@gmail.com>
2025-09-21 22:26:21 +0600

    New logo

---
+++ frontend/components/header/logo.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+
+const Logo = () => {
+   return (
+      <Link href="/" className="flex shrink-0 items-center gap-1">
+         <svg
+            width="30"
+            height="30"
+            viewBox="0 0 52 52"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+         >
+            <path
+               fill-rule="evenodd"
+               clip-rule="evenodd"
+               d="M39 0H26V13H13H0V26V39V52H13H26V39H39H52V26V13V0H39ZM13 39H26V26H39V13H26V26H13V39Z"
+               fill="#155dfb"
+            />
+         </svg>
+
+         <span className="leading-none">
+            <span className="block text-2xl leading-none font-black text-blue-500">
+               MOVIE<span className="text-xl font-light">s</span>
+            </span>
+            <span className="text-secondary -mt-1 block text-end text-sm leading-none font-medium">
+               WORLD
+            </span>
+         </span>
+      </Link>
+   );
+};
+
+export { Logo };
