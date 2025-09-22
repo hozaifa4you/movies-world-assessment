ff0fffa7e0e2eb785dd1a3c38be8969b25efb2ae
Hozafia <hozaifabinmasud@gmail.com>
2025-09-22 09:39:43 +0600

    Add selected movie section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from '@/components/home/hero-section';
 import { RecentSection } from '@/components/home/recent-section';
+import { SelectedMovieSection } from '@/components/home/selected-movie-section';
 
 const HomePage = () => {
    return (
       <main className="w-full">
          <HeroSection />
          <RecentSection />
+         <SelectedMovieSection />
       </main>
    );
 };
