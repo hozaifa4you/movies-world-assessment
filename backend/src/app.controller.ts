9cb6774a8534a7e4bb13c53e458dfdf00c9c0394
Hozafia <hozaifabinmasud@gmail.com>
2025-09-23 16:46:43 +0600

    Get hero content api

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
    constructor(private readonly appService: AppService) {}
 
-   @Get()
-   getHello(): string {
-      return this.appService.getHello();
+   @Get('hero-content')
+   public async getHeroContent() {
+      return this.appService.getHeroContent();
    }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import type { Database } from './database/types';
+import { DB } from './database/database.module';
+import { and, desc, isNotNull, eq, or } from 'drizzle-orm';
+import { movies } from './database/schemas';
 
 @Injectable()
 export class AppService {
-   getHello(): string {
-      return 'Hello World!';
+   constructor(@Inject(DB) private readonly db: Database) {}
+
+   public async getHeroContent() {
+      const moviesResult = await this.db.query.movies.findMany({
+         limit: 5,
+         orderBy: desc(movies.createdAt),
+         where: and(
+            isNotNull(movies.posterUrl),
+            or(
+               eq(movies.status, 'released'),
+               eq(movies.status, 'upcoming'),
+               eq(movies.status, 'post-production'),
+            ),
+         ),
+         columns: {
+            id: true,
+            title: true,
+            posterUrl: true,
+            releaseDate: true,
+            status: true,
+            rating: true,
+            shortDescription: true,
+            genre: true,
+         },
+      });
+
+      return moviesResult;
    }
 }
